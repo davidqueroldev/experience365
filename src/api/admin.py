7859c3f9ff409e54a9c997191782507ef5503611
David Querol Pallarés <morellans@gmail.com>
2025-11-12 22:19:18 +0000

    fixed dependences

--- src/api/admin.py
+++ src/api/admin.py
@@ -12,11 +12,10 @@ from api.models.ActivityImage import ActivityImage
 from flask_admin.contrib.sqla import ModelView
 
 
-
 def setup_admin(app):
     app.secret_key = os.environ.get('FLASK_APP_KEY', 'sample key')
     app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
-    admin = Admin(app, name='4Geeks Admin', template_mode='bootstrap3')
+    admin = Admin(app, name='4Geeks Admin')
 
     # Add your models here, for example this is how we add a the User model to the admin
     admin.add_view(ModelView(User, db.session))
@@ -25,8 +24,8 @@ def setup_admin(app):
     admin.add_view(ModelView(Payments, db.session))
     admin.add_view(ModelView(GlobalRate, db.session))
     admin.add_view(ModelView(Favorite, db.session))
-    admin.add_view(ModelView(ActivityImage, db.session, name='Activity Images', category='Models'))
-
+    admin.add_view(ModelView(ActivityImage, db.session,
+                   name='Activity Images', category='Models'))
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
